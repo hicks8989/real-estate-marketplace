01bfa6588f35f5d80af262bb40d73f283524f3c6
Eric Hicks <eric.hicks.8989@gmail.com>
2020-07-08 15:39:28 -0400

    feat: Created SolnSquareVerifier contract

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,4 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
-
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const SquareVerifier = artifacts.require('SquareVerifier');
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
