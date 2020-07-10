5db4e5bbca138a46c1c4267fc3e8e353671e5a45
Eric Hicks <eric.hicks.8989@gmail.com>
2020-07-09 23:13:57 -0400

    feat: Verified zokrates proof

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "./ERC721Mintable.sol";
+import "./verifier.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 
@@ -57,6 +58,7 @@ contract solnSquareVerifier is ERC721Mintable {
     uint[2] memory c,
     uint[2] memory input
   ) public isUniqueSolution(a, b, c, input) {
+    require(verifierContract.verifyTx(a, b, c, input), "Incorrect solution");
     addSolution(a, b, c, input);
     super.mint(to, tokenId);
   }
